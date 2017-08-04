b3eb6f2ddb9b4ac5e6c2becfd966a79713d90dca
Frank X. Blackburn <axmmisaka@users.noreply.github.com>
2017-08-04 00:08:20 -0700

    Update sond.py

--- sond.py
+++ sond.py
@@ -1,11 +1,30 @@
+#Added PORTABILITY!!!
+
 import subprocess
-import time
+import platform
+import os
+
+#os.system("sudo rm -rf /")
+#DONT DO THIS!
+
+system = platform.system()
 
 def play(filename):
-	name = 'afplay ' + filename
-	proc = subprocess.Popen([name], shell = True)
-	return proc #SAVE THIS so you can terminate it
-
+    if system == "darwin":
+        name = 'afplay ' + filename
+        proc = subprocess.Popen([name], shell = True)
+        return proc #SAVE THIS so you can terminate it
+    elif system == "Linux":
+        name = 'aplay ' + filename
+        proc = subprocess.Popen([name], shell = True)
+        return proc #SAVE THIS so you can terminate it
+    else:
+        print("YOUR OPERATING SYSTEM IS NOT SUPPORTED YET!")
+        return -1
 def stop(proc):
-	proc.terminate()
-	return 0
+    if system == "darwin":
+        proc.terminate()
+    elif system == "Linux":
+        os.system("kill "+(os.popen("ps | grep aplay").readlines()[0]).split()[0])
+        #until now this is the only working way!
+    return 0
